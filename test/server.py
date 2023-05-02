a68728a1e2b1c21b0641373f6d8cb21521a7d236
nachoDRT <iderodrigo@comillas.edu>
2023-05-02 19:26:07 +0200

    Add New Feature
    You can upload videos now to the server.

--- test/server.py
+++ test/server.py
@@ -1,12 +1,26 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, request
 import os
+from werkzeug.utils import secure_filename
 
 server = Flask(__name__)
+server.config["UPLOAD_FOLDER"] = "videos"
+
+if not os.path.exists(server.config["UPLOAD_FOLDER"]):
+    os.makedirs(server.config["UPLOAD_FOLDER"])
 
 
 @server.route("/")
 def index():
     return render_template("upload_video.html")
+
+
+@server.route("/upload", methods=["POST"])
+def upload():
+    video = request.files["video"]
+    video.save(
+        os.path.join(server.config["UPLOAD_FOLDER"], secure_filename(video.filename))
+    )
+    return "Video was uploaded"
 
 
 if __name__ == "__main__":
